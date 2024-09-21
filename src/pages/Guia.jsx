5a5c269a432105fa7f46134c790eaf8af763c177
EduardoChaves12b Chaves <eduardo.custodio@eaportal.org>
2024-09-21 19:32:18 +0000

    Página Guia de Utilização

--- src/pages/Guia.jsx
+++ src/pages/Guia.jsx
@@ -3,16 +3,53 @@ import { GuiaConteudo } from '../styles/Guia.styles'
 import { useState } from 'react';
 import Header from '../components/Header'
 import Botao from "../components/Botao";
+import Configuracao from "/IconConfiguracao.png"
+import Controle from "/IconControle.png"
+import Funcionalidade from "/IconFuncionalidade.png"
 
 export default function Guia() {
   const [darkMode, setDarkMode] = useState(false);
   const toggleDarkMode = () => {
     setDarkMode(!darkMode)
   }
+
+  const [guia1, setGuia1] = useState(false);
+  const toggleGuia1 = () => {
+    setGuia1(!guia1)
+  }
+
+  const [guia2, setGuia2] = useState(false);
+  const toggleGuia2 = () => {
+    setGuia2(!guia2)
+  }
+
+  const [guia3, setGuia3] = useState(false);
+  const toggleGuia3 = () => {
+    setGuia3(!guia3)
+  }
+
   return (
     <GuiaConteudo $darkMode={darkMode}>
       <Header darkMode={darkMode}/>
       <Botao darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+      <h1>Guia de Ultilização</h1>
+      <section id="guias">
+        <button onClick={toggleGuia1} className={guia1 ? 'ativo' : 'oculto'}>
+          <h3>Funcionalidade</h3>
+          <p>O nosso produto monitora a temperatura do ambiente e a umidade do ar de forma contínua e precisa. Após a coleta dos dados, ele processa essas informações e envia um alerta ao usuário por meio do aplicativo.</p>
+          <img src={Funcionalidade} alt="" />
+        </button>
+        <button onClick={toggleGuia2} className={guia2 ? 'ativo' : 'oculto'}>
+          <h3>Controle Intuitivo</h3>
+          <p>Receba alertas em tempo real no seu aplicativo quando as condições de temperatura ou umidade ultrapassarem os limites definidos. Visualize os dados detalhados das medições recentes e históricas. Ajuste facilmente os limites de alerta e as configurações do dispositivo através do aplicativo.</p>
+          <img src={Controle} alt="" />
+        </button>
+        <button onClick={toggleGuia3} className={guia3 ? 'ativo' : 'oculto'}>
+          <h3>Configuração</h3>
+          <p>Acesse as configurações no aplicativo para definir os limites desejados para temperatura e umidade. Estabeleça os valores mínimos e máximos aceitáveis para ambos os parâmetros. Isso permitirá que o dispositivo identifique condições fora dos limites e envie alertas correspondentes.</p>
+          <img src={Configuracao} alt="" />
+        </button>
+      </section>
       <GlobalStyle/>
     </GuiaConteudo>
   )
--- src/styles/Guia.styles.js
+++ src/styles/Guia.styles.js
@@ -5,4 +5,81 @@ export const GuiaConteudo = styled.main`
     transition: .5s;
     min-height: 100vh;
     border: 1.25rem solid ${({ $darkMode }) => ($darkMode ? '#ECF7FE' : '#1A2D7A')};
+    h1{
+        font-family: "Poppins";
+        font-size: 6.125rem;
+        font-weight: 400;
+        text-align: center;
+        color: ${({ $darkMode }) => ($darkMode ? '#ECF7FE' : '#1A2D7A')};
+        padding-top: 3.5rem;
+    }
+
+    #guias{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        min-height: calc(100vh - 30rem);
+        gap: 1rem;
+        padding-inline: 1rem;
+
+        .oculto{
+            background-image: linear-gradient(to bottom, #1A2D7A, #3053E0);
+        }
+
+        .ativo{
+            background-image: linear-gradient(to bottom, #FBFDFF, rgba(20, 101, 187, .625));
+        }
+        
+        button{
+            max-width: 36.75rem;
+            min-height: 47.5rem;
+            width: 100%;
+            border: none;
+            border-radius: 15px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-evenly;
+            align-items: center;
+            cursor: pointer;
+            transition: 0.5s;
+            padding-inline: 2.5rem;
+
+            h3{
+                font-family: "Open Sans";
+                font-weight: 400;
+                font-size: 3.5rem;
+            }
+
+            img{
+                max-width: 17.25rem;
+            }
+
+            p{
+                font-family: "Righteous";
+                font-size: 1.75rem;
+                color: #1A2D7A;
+                text-align: left;
+            }
+        }
+
+        button:active{
+            transform: scale(.9);
+        }
+    }
+
+    .oculto p{
+        display: none;
+    }
+
+    .ativo h3{
+        color: #1A2D7A;
+    }
+
+    .oculto h3{
+        color: #ECF7FE;
+    }
+
+    .oculto img{
+        filter: invert(100%) brightness(1000%);     
+    }
 `
