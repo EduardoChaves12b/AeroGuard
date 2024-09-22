bacf2750c1285f0f510c1f909cdd36e595f7c57b
EduardoChaves12b Chaves <eduardo.custodio@eaportal.org>
2024-09-22 02:40:45 +0000

    Página de Referências

--- src/pages/Referencias.jsx
+++ src/pages/Referencias.jsx
@@ -1,8 +1,10 @@
 import { GlobalStyle } from "../styles/global";
-import {ReferenciasConteudo} from '../styles/Referencias.styles'
+import {Artigo, ReferenciasConteudo} from '../styles/Referencias.styles'
 import { useState } from 'react';
 import Header from '../components/Header'
 import Botao from "../components/Botao";
+import Baixar from "/Baixar.png"
+import PDF from "/PDF.png"
 
 export default function Referencias() {
   const [darkMode, setDarkMode] = useState(false);
@@ -13,6 +15,41 @@ export default function Referencias() {
     <ReferenciasConteudo $darkMode={darkMode}>
       <Header darkMode={darkMode}/>
       <Botao darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+      <h1>Referências Bibliográficas</h1>
+      <section id="artigos">
+        <Artigo $darkMode={darkMode}>
+          <h2>1</h2>
+          <a href="https://repositorio.pucgoias.edu.br/jspui/handle/123456789/4892">
+            Análise da eficácia de medida caseira e umidificador na melhora da umidade do ar.
+            <img src={PDF} alt="PDF" />
+            <img className={darkMode ? 'branco' : ''} src={Baixar} alt="Baixar ícone" />
+          </a>
+        </Artigo>
+        <Artigo $darkMode={darkMode}>
+          <h2>2</h2>
+          <a href="">
+            Utilização de prototipagem arduino para controlar a umidade em ambientes climatizados.
+            <img src={PDF} alt="PDF" />
+            <img className={darkMode ? 'branco' : ''} src={Baixar} alt="Baixar ícone" />
+          </a>
+        </Artigo>
+        <Artigo $darkMode={darkMode}>
+          <h2>3</h2>
+          <a href="">
+            Associação entre poluição do ar e da asma com níves de poluetes dentro do recomendável.
+            <img src={PDF} alt="PDF" />
+            <img className={darkMode ? 'branco' : ''} src={Baixar} alt="Baixar ícone" />
+          </a>
+        </Artigo>
+        <Artigo $darkMode={darkMode}>
+          <h2>4</h2>
+          <a href="">
+            Poluição ambiental como factor de risco para o aumento de asma brônquica?
+            <img src={PDF} alt="PDF" />
+            <img className={darkMode ? 'branco' : ''} src={Baixar} alt="Baixar ícone" />
+          </a>
+        </Artigo>
+      </section>
       <GlobalStyle/>
     </ReferenciasConteudo>
   )
--- src/styles/Referencias.styles.js
+++ src/styles/Referencias.styles.js
@@ -3,7 +3,79 @@ import styled from "styled-components"
 export const ReferenciasConteudo = styled.main`
     background: ${({ $darkMode }) => ($darkMode ? '#1A2D7A' : '#ECF7FE')};
     transition: .5s;
-    .oculto{
-        display: none;
+    min-height: 100vh;
+    border: 1.25rem solid ${({ $darkMode }) => ($darkMode ? '#ECF7FE' : '#1A2D7A')};
+    h1{
+        font-family: "Poppins";
+        font-size: 6.125rem;
+        font-weight: 400;
+        text-align: center;
+        color: ${({ $darkMode }) => ($darkMode ? '#ECF7FE' : '#1A2D7A')};
+        padding-top: 3.5rem;
+    }
+
+    #artigos{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        margin-left: 10rem;
+        min-height: calc(100vh - 30rem);
+        max-width: 150rem;
+    }
+
+`
+export const Artigo = styled.div`
+    display: flex;
+
+    h2{
+        font-family: "Righteous";
+        font-weight: 400;
+        font-size: 5rem;
+        background: ${({ $darkMode }) => ($darkMode ? '#ECF7FE' : '#1A2D7A')};
+        color: ${({ $darkMode }) => ($darkMode ? '#1A2D7A' : '#ECF7FE')};
+        width: 5rem;
+        height: 5.25rem;
+        border-radius: 5px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+        z-index: 1;
+        margin-right: 5.5rem;
+    }
+
+    h2::after{
+        content: '';
+        position: absolute;
+        left: 100%;
+        top: 45%;
+        width: 4.75rem;
+        height: .75rem;
+        background-color: ${({ $darkMode }) => ($darkMode ? '#ECF7FE' : '#1A2D7A')};
+    }
+
+    &::before {
+        content: '';
+        position: absolute;
+        left: 13.325rem;
+        top: 30rem;
+        bottom: 15%;
+        width: 1rem;
+        background-color: ${({ $darkMode }) => ($darkMode ? '#ECF7FE' : '#1A2D7A')};
+        z-index: 0;
+    }
+
+    a{
+        font-family: "Open Sans";
+        font-size: 2rem;
+        color: ${({ $darkMode }) => ($darkMode ? '#ECF7FE' : '#1A2D7A')};
+        text-align: center;
+        display: flex;
+        align-items: center;
+        max-width: 95rem;
+    }
+
+    .branco{
+        filter: invert(100%) brightness(1000%);     
     }
 `
